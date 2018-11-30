e46b5c73819aeaf0fa0e7596f80d396945f395ff
Janos Gonye <janos.gonye@gmail.com>
2018-11-30 23:05:07 +0100

    basic project structure

---
+++ app/services/auth.py
@@ -0,0 +1,22 @@
+class AuthService:
+	"""Authentication Service for handling registration, login etc."""
+	email = None
+	token = None
+
+	def __init__(self):
+		pass
+
+	def registrate(email, password):
+		pass
+
+	def delete_registration():
+		pass
+
+	def login(email, password):
+		pass
+
+	def logout():
+		pass
+
+	def store_token():
+		pass
---
+++ app/services/http.py
@@ -0,0 +1,23 @@
+class HttpService:
+	"""
+	Basic HTTP Service
+	Responsible for Sending HTTP Requests and Receiving Responses
+	"""
+
+	def __init__(self):
+		pass
+
+	def get(path, query_params={}, host=, port=80, protocol="https"):
+		pass
+
+	def post(path, body=None, query_params={}, host=, port=80, protocol="https"):
+		pass
+
+	def put(path, body=None, query_params={}, host=, port=80, protocol="https"):
+		pass
+
+	def patch(path, body=None, query_params={}, host=, port=80, protocol="https"):
+		pass
+
+	def delete(path, query_params={}, host=, port=80, protocol="https"):
+		pass
