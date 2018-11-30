1a3b60cf8283064c1e868b16aa10791709022f40
János Gönye <janos.gonye@gmail.com>
2018-11-30 23:05:07 +0100

    basic project structure

---
+++ app/services/category.py
@@ -0,0 +1,17 @@
+class CategoryService:
+	"""Category Service for handling CRUD operations on categories"""
+
+	def create(category):
+		pass
+
+	def get_all():
+		pass
+
+	def get(id):
+		pass
+
+	def update(category):
+		pass
+
+	def delete(category):
+		pass
---
+++ app/services/transaction.py
@@ -0,0 +1,17 @@
+class TransactionService:
+	"""Transaction Service for handling CRUD operations on transactions"""
+
+	def create(transaction):
+		pass
+
+	def get_all():
+		pass
+
+	def get(id):
+		pass
+
+	def update(transaction):
+		pass
+
+	def delete(transaction):
+		pass
