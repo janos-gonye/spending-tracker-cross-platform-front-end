4e6288b9e84dfb6e2b4abb66aeca25d84785fdf3
János Gönye <janos.gonye@gmail.com>
2018-12-08 22:15:23 +0100

    errorpopup renamed to infopopup | _attach_token decorator fixed | show user registration and login error | inform user when registration email sent

---
+++ app/uix/popups/info/__init__.py
@@ -0,0 +1,13 @@
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
+
+
+Builder.load_file('uix/popups/info/infopopup.kv')
+
+
+class InfoPopup(Popup):	
+
+	def __init__(self, message, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.title = kwargs['title']
+		self.label.text = "[i]{message}[/i]".format(message=message)
