156d27b39bb54ebb1eea24190c32797b9be66864
Janos Gonye <janos.gonye@gmail.com>
2019-03-31 22:00:58 +0200

    Add back and new category button UI elements to 'CategoryScreen'

--- app/screens/category/__init__.py
+++ app/screens/category/__init__.py
@@ -7,7 +7,7 @@ Builder.load_file('screens/category/category.kv')
 
 class CategoryScreen(Screen):
 	"""
-	The user can manage his categories and subcategories.
+	The user can manage his or her categories and subcategories.
 	"""
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
