ea95b38a91d396450435ebaf8d51b9e4291e10f2
János Gönye <janos.gonye@gmail.com>
2019-03-31 23:44:09 +0200

    Category model implemented

--- app/models/category.py
+++ app/models/category.py
@@ -1,2 +1,41 @@
+from datetime import datetime
+from datetime import timezone
+
+
 class Category:
-	pass
+
+	def __init__(self, id, title, description, parent, created_at, updated_at):
+		"""
+		Parent can be both 'Category' instance or JSON object.
+		"""
+		self.id = id
+		self.title = title
+		self.description = description or ""
+		if parent is None:
+			self.parent = None
+		elif type(parent) is Category:
+			self.parent = parent
+		else:
+			self.parent = Category.from_json(parent)
+		self.created_at = created_at
+		self.updated_at = updated_at
+
+	def as_dict(self):
+		return {
+			'id': self.id,
+			'description': self.description,
+			'parent': self.parent,
+			'created_at': self.created_at,
+			'updated_at': self.updated_at,
+		}
+
+	@staticmethod
+	def from_json(json):
+		return Category(
+			id=json['id'],
+			title=json['title'],
+			description=json['description'],
+			parent=json['parent'],
+			created_at=json['created_at'],
+			updated_at=json['updated_at']
+		)
