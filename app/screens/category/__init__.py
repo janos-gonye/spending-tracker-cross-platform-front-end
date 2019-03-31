bf6db891e2a312009a669e2ea69b465c4dd7447d
Janos Gonye <janos.gonye@gmail.com>
2019-03-31 22:34:15 +0200

    get categories from server when entering 'CategoryScreen'

--- app/screens/category/__init__.py
+++ app/screens/category/__init__.py
@@ -1,6 +1,10 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
+from services.category import CategoryService
+from services.exceptions import ConnectionError_
+from uix.popups.info import InfoPopup
+
 
 Builder.load_file('screens/category/category.kv')
 
@@ -11,3 +15,11 @@ class CategoryScreen(Screen):
 	"""
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		self.service = CategoryService()
+
+	def on_pre_enter(self):
+		try:
+			self.categories = self.service.get_all()
+			print(self.categories)
+		except ConnectionError_ as err:
+			InfoPopup(title='Connection Error', message=str(err)).open()
--- app/services/category.py
+++ app/services/category.py
@@ -1,11 +1,24 @@
-class CategoryService:
+import constants
+
+from services.api import ApiService
+from services.exceptions import ConnectionError_
+
+
+class CategoryService(ApiService):
 	"""Category Service for handling CRUD operations on categories"""
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
 
 	def create(self, category):
 		pass
 
 	def get_all(self):
-		pass
+		response = super().get(path=constants.API_CATEGORIES)
+		payload = response[0]
+		if payload is None:
+			raise ConnectionError_(response[1])
+		return payload
 
 	def get(self, id):
 		pass
