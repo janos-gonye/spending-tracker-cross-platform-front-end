d1172e538b46efd20e00417475b9070a8bba636d
Janos Gonye <janos.gonye@gmail.com>
2019-04-01 02:04:52 +0200

    basic scrollable category list added

--- app/screens/category/__init__.py
+++ app/screens/category/__init__.py
@@ -3,6 +3,7 @@ from kivy.uix.screenmanager import Screen
 
 from services.category import CategoryService
 from services.exceptions import ConnectionError_
+from uix.widgets.category_box import CategoryBox
 from uix.popups.info import InfoPopup
 
 
@@ -16,10 +17,25 @@ class CategoryScreen(Screen):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.service = CategoryService()
+		self.conn_error = None
+		self._init_scroll_list()
 
 	def on_pre_enter(self):
 		try:
 			self.categories = self.service.get_all()
-			print(self.categories)
 		except ConnectionError_ as err:
-			InfoPopup(title='Connection Error', message=str(err)).open()
+			self.conn_error = err
+
+	def on_enter(self):
+		if self.conn_error:
+			InfoPopup(title='Connection Error', message=str(self.conn_error)).open()
+		else:
+			self._list_categories()
+
+	def _list_categories(self):
+		for category in self.categories:
+			self.list.add_widget(CategoryBox(category=category))
+
+	def _init_scroll_list(self):
+		# Necessary to scroll
+		self.list.bind(minimum_height=self.list.setter('height'))
