08350d978e51d83f98b52ee5438cca7fe00158a1
János Gönye <janos.gonye@gmail.com>
2019-04-01 02:03:00 +0200

    category box implemented

---
+++ app/uix/widgets/category_box/__init__.py
@@ -0,0 +1,14 @@
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+
+
+Builder.load_file('uix/widgets/category_box./category_box.kv')
+
+
+class CategoryBox(Widget):
+
+	def __init__(self, category, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.category = category
+		self.title.text = f"[b]{category.title}[/b]"
+		self.description.text = f"[b]{category.description}[/b]"
