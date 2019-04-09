3c212c15f4ab63c5925489875b68ae8e7a42db6b
János Gönye <janos.gonye@gmail.com>
2019-04-09 15:06:00 +0200

    scrolling categories works

--- app/uix/widgets/category_box/__init__.py
+++ app/uix/widgets/category_box/__init__.py
@@ -1,11 +1,12 @@
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 
 
 Builder.load_file('uix/widgets/category_box/category_box.kv')
 
 
-class CategoryBox(Widget):
+class CategoryBox(BoxLayout):
 
 	def __init__(self, category, *args, **kwargs):
 		super().__init__(*args, **kwargs)
