eb0a00daafc94d74a0f577e31ef6a3b019aa4b2c
János Gönye <janos.gonye@gmail.com>
2019-04-09 15:51:40 +0200

    'CategoryCreateScreen' skeleton created

---
+++ app/screens/category_create/__init__.py
@@ -0,0 +1,9 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+
+Builder.load_file('screens/category_create/category_create.kv')
+
+
+class CategoryCreateScreen(Screen):
+    pass
