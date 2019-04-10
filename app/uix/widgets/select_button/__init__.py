1086f9d497904d79b273121dcec4716a2d022104
János Gönye <janos.gonye@gmail.com>
2019-04-10 13:28:51 +0200

    'SelectButton' prototype added

---
+++ app/uix/widgets/select_button/__init__.py
@@ -0,0 +1,20 @@
+from functools import partial
+
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+
+from kivy.properties import StringProperty
+
+
+class SelectButton(Button):
+    attr_name = StringProperty()
+
+    def init(self, elements):
+        dropdown = DropDown()
+        for elm in elements:
+            text = getattr(elm, self.attr_name)
+            btn = Button(text=text, size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            dropdown.add_widget(btn)
+        dropdown.bind(on_select=lambda instance, x: setattr(self, 'text', x))
+        self.bind(on_release=dropdown.open)
