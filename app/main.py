5cac72cfbbe97b0d108a3f2fddd4ee92457ef191
János Gönye <janos.gonye@gmail.com>
2019-04-10 10:51:12 +0200

    category screens package created

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import SwapTransition
 
-from screens.category import CategoryScreen
-from screens.category_create import CategoryCreateScreen
+from screens.category.list import CategoryListScreen
+from screens.category.create import CategoryCreateScreen
 from screens.login import LoginScreen
 from screens.main import MainScreen
 from screens.registration import RegistrationScreen
@@ -19,7 +19,7 @@ class SpendingTrackerApp(App):
 		screen_manager.add_widget(LoginScreen(name='login'))
 		screen_manager.add_widget(RegistrationScreen(name='registration'))
 		screen_manager.add_widget(MainScreen(name='main'))
-		screen_manager.add_widget(CategoryScreen(name='category'))
+		screen_manager.add_widget(CategoryListScreen(name='category'))
 		screen_manager.add_widget(CategoryCreateScreen(name='category_create'))
 		return screen_manager
 
--- app/screens/category/create/__init__.py
+++ app/screens/category/create/__init__.py
@@ -2,7 +2,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
 
-Builder.load_file('screens/category_create/category_create.kv')
+Builder.load_file('screens/category/create/category_create.kv')
 
 
 class CategoryCreateScreen(Screen):
