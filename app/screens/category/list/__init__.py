1bd17532b43b12556d986a6c5a4dd4fd897eb998
Gönye János <johnny@andrews.hu>
2019-04-10 11:29:57 +0200

    'CategoryListScreen' and 'CategoryCreateScreen' inherits from base 'CategoryScreen' from now on

--- app/screens/category/list/__init__.py
+++ app/screens/category/list/__init__.py
@@ -1,35 +1,24 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
-from services.category import CategoryService
-from services.exceptions import ConnectionError_
+from screens.category.base import CategoryScreen
 from uix.widgets.category_box import CategoryBox
-from uix.popups.info import InfoPopup
 
 
 Builder.load_file('screens/category/list/category_list.kv')
 
 
-class CategoryListScreen(Screen):
+class CategoryListScreen(CategoryScreen):
 	"""
 	The user can manage his or her categories and subcategories.
 	"""
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.service = CategoryService()
-		self.conn_error = None
 		self._init_scroll_list()
 
-	def on_pre_enter(self):
-		try:
-			self.categories = self.service.get_all()
-		except ConnectionError_ as err:
-			self.conn_error = err
-
 	def on_enter(self):
-		if self.conn_error:
-			InfoPopup(title='Connection Error', message=str(self.conn_error)).open()
-		else:
+		super().on_enter()
+		if not self.conn_error:
 			self._list_categories()
 
 	def on_leave(self):
