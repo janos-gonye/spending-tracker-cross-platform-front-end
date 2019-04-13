36b86d34b4f33d23a72c16df51621ee6201ecd47
Janos Gonye <janos.gonye@gmail.com>
2019-04-14 01:20:56 +0200

    removed non-used contant 'API_AUTH_DELETE'

--- app/constants.py
+++ app/constants.py
@@ -7,5 +7,4 @@ API_DEFAULT_KEY = 'message'
 # API Endpoints
 API_AUTH_LOGIN = API_PATH + '/auth/login'
 API_AUTH_REGISTRATION = API_PATH + '/auth/registration'
-API_AUTH_DELETE = API_PATH + '/auth/registration'
 API_CATEGORIES = API_PATH + '/categories'
