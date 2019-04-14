7bd85b24f15cb709a83dae16e4fdb538c294cd28
János Gönye <janos.gonye@gmail.com>
2019-04-14 12:39:36 +0200

    'SelectButton's selection algorithm fixed

--- app/uix/widgets/select_button/__init__.py
+++ app/uix/widgets/select_button/__init__.py
@@ -6,20 +6,30 @@ from kivy.uix.dropdown import DropDown
 
 class SelectButton(Button):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._selected = None
+        self._elements = []
+        self._init_dropdown()
+
     def init(self, elements):
         self._elements = elements
-        dropdown = DropDown()
         for elm in elements:
             text = getattr(elm, self.attr_name)
             btn = Button(text=text, size_hint_y=None, height=44)
-            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-            dropdown.add_widget(btn)
-        dropdown.bind(on_select=lambda instance, x: setattr(self, 'text', x))
-        self.bind(on_release=dropdown.open)
+            btn.bind(on_release=partial(self._on_select, elm=elm))
+            self._dropdown.add_widget(btn)
 
     @property
     def selected(self):
-        for elm in self._elements:
-            if getattr(elm, self.attr_name) == self.text:
-                return elm
-        return None
+        return self._selected
+
+    def _init_dropdown(self):
+        self._dropdown = DropDown()
+        self._dropdown.bind(
+            on_select=lambda instance, x: setattr(self, 'text', x))
+        self.bind(on_release=self._dropdown.open)
+
+    def _on_select(self, btn, elm):
+        self._dropdown.select(btn.text)
+        self._selected = elm
