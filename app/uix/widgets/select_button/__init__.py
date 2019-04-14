1012c5c678754b4a51640f2e47681fd728f5611a
János Gönye <janos.gonye@gmail.com>
2019-04-14 13:02:54 +0200

    optional allow null selection added to 'SelectButton'

--- app/uix/widgets/select_button/__init__.py
+++ app/uix/widgets/select_button/__init__.py
@@ -3,8 +3,14 @@ from functools import partial
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
 
+from kivy.properties import BooleanProperty
+from kivy.properties import StringProperty
+
 
 class SelectButton(Button):
+    allow_null = BooleanProperty(False)
+    attr_name = StringProperty('id')
+    null_text = StringProperty('None')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -13,15 +19,19 @@ class SelectButton(Button):
         self._init_dropdown()
 
     def init(self, elements):
+        self._dropdown.clear_widgets()
         self._elements = elements
+        if self.allow_null:
+            self._add_null_btn()
         for elm in elements:
             text = getattr(elm, self.attr_name)
             btn = Button(text=text, size_hint_y=None, height=44)
-            btn.bind(on_release=partial(self._on_select, elm=elm))
+            btn.bind(on_release=partial(self._on_select_elm, elm=elm))
             self._dropdown.add_widget(btn)
 
     @property
     def selected(self):
+        print(self._selected)
         return self._selected
 
     def _init_dropdown(self):
@@ -30,6 +40,15 @@ class SelectButton(Button):
             on_select=lambda instance, x: setattr(self, 'text', x))
         self.bind(on_release=self._dropdown.open)
 
-    def _on_select(self, btn, elm):
+    def _on_select_elm(self, btn, elm):
         self._dropdown.select(btn.text)
         self._selected = elm
+
+    def _add_null_btn(self):
+        btn = Button(text=self.null_text, size_hint_y=None, height=44)
+        btn.bind(on_release=self._on_null_select)
+        self._dropdown.add_widget(btn)
+
+    def _on_null_select(self, btn):
+        self._dropdown.select(btn.text)
+        self._selected = None
