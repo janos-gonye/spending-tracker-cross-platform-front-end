5ea280149e620717f10d3a2e1ae8c8768aa50dd2
János Gönye <janos.gonye@gmail.com>
2019-04-14 12:15:17 +0200

    'selected' property added to 'SelectButton'

--- app/uix/widgets/select_button/__init__.py
+++ app/uix/widgets/select_button/__init__.py
@@ -7,6 +7,7 @@ from kivy.uix.dropdown import DropDown
 class SelectButton(Button):
 
     def init(self, elements):
+        self._elements = elements
         dropdown = DropDown()
         for elm in elements:
             text = getattr(elm, self.attr_name)
@@ -15,3 +16,10 @@ class SelectButton(Button):
             dropdown.add_widget(btn)
         dropdown.bind(on_select=lambda instance, x: setattr(self, 'text', x))
         self.bind(on_release=dropdown.open)
+
+    @property
+    def selected(self):
+        for elm in self._elements:
+            if getattr(elm, self.attr_name) == self.text:
+                return elm
+        return None
