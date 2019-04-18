765e1f9812597a03e9051ddd9e4369e2cf713972
Janos Gonye <janos.gonye@gmail.com>
2019-04-18 22:53:42 +0200

    fetch categories and rerender category list after removing a category

--- app/screens/category/base.py
+++ app/screens/category/base.py
@@ -10,16 +10,22 @@ class CategoryScreen(Screen):
     def __init__(self,  *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.service = CategoryService()
-        self.conn_error = None
 
     def on_pre_enter(self):
+        self.fetch_categories()
+
+    def on_enter(self):
+        self.show_if_error()
+
+    def fetch_categories(self):
         try:
             self.categories = self.service.get_all()
+            self.conn_error = None
         except ConnectionError_ as err:
             self.conn_error = err
             self.categories = []
-    
-    def on_enter(self):
+
+    def show_if_error(self):
         if self.conn_error:
             InfoPopup(title='Error',
                       message=str(self.conn_error)).open()
