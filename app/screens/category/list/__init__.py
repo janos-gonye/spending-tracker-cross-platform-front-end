1174b45e63a29815b8b2201a6e0122d9c453f6d5
Janos Gonye <janos.gonye@gmail.com>
2019-04-18 21:22:03 +0200

    methods bound to 'CategoryBox' on_release property

--- app/screens/category/list/__init__.py
+++ app/screens/category/list/__init__.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
@@ -14,20 +16,31 @@ class CategoryListScreen(CategoryScreen):
 	"""
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self._init_scroll_list()
+		self.init_scroll_list()
 
 	def on_enter(self):
 		super().on_enter()
 		if not self.conn_error:
-			self._list_categories()
+			self.list_categories()
 
 	def on_leave(self):
 		self.list.clear_widgets()
 
-	def _list_categories(self):
-		for category in self.categories:
-			self.list.add_widget(CategoryBox(category=category))
+	def delete_category(self, category):
+		print(category)
 
-	def _init_scroll_list(self):
+	def update_category(self, category):
+		print(category)
+
+	def list_categories(self):
+		for category in self.categories:
+			cat_box = CategoryBox(category=category)
+			cat_box.remove_btn.on_release = partial(self.delete_category,
+													category=category)
+			cat_box.update_btn.on_release = partial(self.update_category,
+													category=category)
+			self.list.add_widget(cat_box)
+
+	def init_scroll_list(self):
 		# Necessary to scroll
 		self.list.bind(minimum_height=self.list.setter('height'))
