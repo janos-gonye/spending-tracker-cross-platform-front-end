93fc85ba0497383fb0e85d56a6ff1043028e11e6
János Gönye <janos.gonye@gmail.com>
2019-04-22 01:33:18 +0200

    'TransactionListScreen' now inherits from 'CategoryScreen'

--- app/screens/transaction/list/__init__.py
+++ app/screens/transaction/list/__init__.py
@@ -1,9 +1,24 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
+from screens.category.base import CategoryScreen
+from uix.popups.info import InfoPopup
+
 
 Builder.load_file('screens/transaction/list/transaction_list.kv')
 
 
-class TransactionListScreen(Screen):
-	pass
+class TransactionListScreen(CategoryScreen):
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.init_scroll_list()
+
+	def on_enter(self):
+		super().on_enter()
+		if not self.conn_error:
+			self.filter.init(categories=self.categories)
+
+	def init_scroll_list(self):
+		# Necessary to scroll
+		self.list.bind(minimum_height=self.list.setter('height'))
