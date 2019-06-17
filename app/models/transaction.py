831e50123645ba56f34ad3e930f7530ed574c8ba
Gönye János <johnny@andrews.hu>
2019-06-17 14:33:43 +0200

    'Transaction' model implemented

--- app/models/transaction.py
+++ app/models/transaction.py
@@ -1,2 +1,38 @@
+from utils.date import timestamp2datetime
+from utils.date import datetime2timestamp
+
+from models.category import Category
+
+
 class Transaction:
-	pass
+
+	def __init__(self, id, amount, comment, category, processed_at):
+		"""
+		Category can be both 'Category' instance or JSON object.
+		"""
+		self.id = id
+		self.amount = amount
+		self.comment = comment
+		if isinstance(category, Category):
+			self.category = category
+		else:
+			self.category = Category.from_json(json=category)
+		self.processed_at = timestamp2datetime(timestamp=processed_at)
+
+	def as_json(self):
+		return {
+			'id': self.id,
+			'amount': self.amount,
+			'comment': self.comment,
+			'processed_at': datetime2timestamp(date=self.processed_at),
+		}
+
+	@staticmethod
+	def from_json(json):
+		return Transaction(
+			id=json['id'],
+			comment=json['comment'],
+			amount=json['amount'],
+			category=json['category'],
+			processed_at=json['processed_at'],
+		)
