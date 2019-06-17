22369229169a0010277e475d137355bac9df1233
Gönye János <johnny@andrews.hu>
2019-06-17 14:36:21 +0200

    'TransactionService.get_all' implemented

--- app/services/transaction.py
+++ app/services/transaction.py
@@ -1,17 +1,41 @@
-class TransactionService:
+import constants
+
+from models.transaction import Transaction
+from services.api import ApiService
+from services.exceptions import ConnectionError_
+
+
+class TransactionService(ApiService):
 	"""Transaction Service for handling CRUD operations on transactions"""
 
-	def create(transaction):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+	def create(self, category, transaction):
 		pass
 
-	def get_all():
+	def get_all(self, category):
+		cat_id = '*' if category is None else category.id
+		path = self._get_path(cat_id=cat_id)
+		payload, error = super().get(path=path)
+		if payload is None:
+			raise ConnectionError_(error)
+		result = []
+		for trans_json in payload['transactions']:
+			result += [Transaction.from_json(json=trans_json)]
+		return result
+
+	def get(self, category, transaction_id):
 		pass
 
-	def get(id):
+	def update(self, category, transaction):
 		pass
 
-	def update(transaction):
+	def delete(self, cateogry, transaction):
 		pass
 
-	def delete(transaction):
-		pass
+	def _get_path(self, cat_id, trans_id=None):
+		path = constants.API_TRANACTIONS.format(cat_id=cat_id)
+		if trans_id:
+			path += f"/{trans_id}"
+		return path
