4da6d676d033a22c3ca0a138012657b7a9e4d0b7
János Gönye <janos.gonye@gmail.com>
2019-06-17 15:08:16 +0200

    Transaction boxes appear in the list of 'TransactionListScreen' when pushing the 'List' button

--- app/uix/widgets/transaction_box/__init__.py
+++ app/uix/widgets/transaction_box/__init__.py
@@ -1,8 +1,6 @@
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 
-from utils.date import timestamp2datetime
-
 
 Builder.load_file('uix/widgets/transaction_box/transaction_box.kv')
 
@@ -14,6 +12,5 @@ class TransactionBox(BoxLayout):
 		self.category.text = f"[b]{transaction.category.history}[/b]"
 		self.comment.text = f"[b]{transaction.comment}[/b]"
 		self.amount = f"[b]{transaction.amount}[/b]"
-		date = timestamp2datetime(transaction.processed_at)
-		date_str = date.strftime("%Y.%m.%d.")
-		self.date.text = f"[b]{date_str}[/b]"
+		date = transaction.processed_at.strftime("%Y.%m.%d.")
+		self.date.text = f"[b]{date}[/b]"
