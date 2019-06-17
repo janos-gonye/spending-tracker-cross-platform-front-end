df5222bc312aca83fbbfb7ce6864d8a00d8674b1
János Gönye <janos.gonye@gmail.com>
2019-06-17 14:39:26 +0200

    get transactions when pushing the 'List' button on 'TransactionListScreen'

--- app/screens/transaction/list/__init__.py
+++ app/screens/transaction/list/__init__.py
@@ -2,6 +2,8 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
 from screens.category.mixins import FetchCategoriesMixin
+from services.transaction import TransactionService
+from services.exceptions import ConnectionError_
 from uix.popups.info import InfoPopup
 
 
@@ -13,6 +15,7 @@ class TransactionListScreen(FetchCategoriesMixin, Screen):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.init_scroll_list()
+		self.service = TransactionService()
 
 	def on_pre_enter(self):
 		self.fetch_categories()
@@ -23,6 +26,16 @@ class TransactionListScreen(FetchCategoriesMixin, Screen):
 					  message=str(self.cat_conn_error)).open()
 		else:
 			self.filter.init(categories=self.categories)
+
+	def list_transactions(self):
+		cat = self.filter.selected_category
+		print(self.fetch_transactions(category=cat))
+
+	def fetch_transactions(self, category):
+		try:
+			return self.service.get_all(category=category)
+		except ConnectionError_ as err:
+			InfoPopup(title='Error', message=str(err)).open()
 
 	def init_scroll_list(self):
 		# Necessary to scroll
