13b3fb7dc795ab8583bdf5494897a32ec925917b
Gönye János <johnny@andrews.hu>
2019-06-18 15:44:39 +0200

    'NumericInput' implemented

---
+++ app/uix/widgets/numeric_input/__init__.py
@@ -0,0 +1,15 @@
+import re
+
+from kivy.uix.textinput import TextInput
+
+
+class NumericInput(TextInput):
+
+    pat = re.compile('[0-9]')
+    def insert_text(self, substring, from_undo=False):
+        pat = self.pat
+        if re.match(pat, substring):
+            s = substring
+        else:
+            s = ''
+        return super().insert_text(s, from_undo=from_undo)
