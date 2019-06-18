9e48ab3bddd7e5709ca5561586bf8a65e9da15d3
Gönye János <johnny@andrews.hu>
2019-06-18 15:46:20 +0200

    fetch categories when entering 'TransactionCreateScreen' | init category selector

--- app/screens/transaction/create/__init__.py
+++ app/screens/transaction/create/__init__.py
@@ -1,11 +1,23 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
+from screens.category.mixins import FetchCategoriesMixin
+from uix.popups.info import InfoPopup
+
 
 Builder.load_file('screens/transaction/create/transaction_create.kv')
 
 
-class TransactionCreateScreen(Screen):
+class TransactionCreateScreen(FetchCategoriesMixin, Screen):
+
+    def on_pre_enter(self):
+        self.fetch_categories()
+
+    def on_enter(self):
+        if self.cat_conn_error:
+            InfoPopup(title='Error', message=str(self.cat_conn_error)).open()
+        else:
+            self.category_selector.init(elements=self.categories)
 
     def submit(self):
         pass
