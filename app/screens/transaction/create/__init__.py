32a3c2f8336310be5db5b36f0c175ef04d0d5da0
Gönye János <johnny@andrews.hu>
2019-06-18 17:53:30 +0200

    create transaction when submitting the screen

--- app/screens/transaction/create/__init__.py
+++ app/screens/transaction/create/__init__.py
@@ -3,6 +3,8 @@ from kivy.uix.screenmanager import Screen
 
 from models.transaction import Transaction
 from screens.category.mixins import FetchCategoriesMixin
+from services.transaction import TransactionService
+from services.exceptions import ConnectionError_
 from uix.popups.info import InfoPopup
 
 
@@ -10,6 +12,10 @@ Builder.load_file('screens/transaction/create/transaction_create.kv')
 
 
 class TransactionCreateScreen(FetchCategoriesMixin, Screen):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.service = TransactionService()
 
     def on_pre_enter(self):
         self.fetch_categories()
@@ -25,14 +31,33 @@ class TransactionCreateScreen(FetchCategoriesMixin, Screen):
                 pass
 
     def submit(self):
-        self.check_fields()
+        if self.check_fields() == False:
+            return
+        transaction = Transaction(
+            id=None,
+            amount=self.number_input.text,
+            comment=self.comment_input.text,
+            category=self.category_selector.selected,
+            processed_at=self.date_picker.timestamp)
+        try:
+            created = self.service.create(transaction=transaction)
+        except ConnectionError_ as err:
+            InfoPopup(title='Error', message=str(err)).open()
+            return None
+        InfoPopup(title="Success",
+                  message="Transaction Successfully Created").open()
+        self.reset_fields()
+        self.manager.current = 'main'
+        return created
 
     def check_fields(self):
-        if self.number_input.text == "" or self.comment_input.text == "" or\
+        if self.number_input.text == "" or\
            self.category_selector.selected is None:
             InfoPopup(title='Error', message="All fields required.").open()
+            return False
+        return True
 
-    def on_leave(self):
+    def reset_fields(self):
         self.number_input.text = ""
         self.comment_input.text = ""
         self.date_picker.reset_today()
