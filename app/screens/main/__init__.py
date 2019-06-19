1d20e2b69de681304a038f6d50bbeef956121a18
Gönye János <johnny@andrews.hu>
2019-06-19 11:16:30 +0200

    'TransactionCreateScreen' now switches back to 'MainScreen' or 'TransactionListScreen' when pushing the 'Cancel' button according to which screen the user came from

--- app/screens/main/__init__.py
+++ app/screens/main/__init__.py
@@ -2,11 +2,13 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from uix.actionbars.app import AppActionBar
 
+from screens.mixins import AdaptiveCancelForTransactionCreateScreenMixin
+
 
 Builder.load_file('screens/main/main.kv')
 
 
-class MainScreen(Screen):
+class MainScreen(AdaptiveCancelForTransactionCreateScreenMixin, Screen):
 	"""
 	This is the screen that the user sees
 	after successfully logging in or already logged in.
