007c3f8cb0bcac23da7e3ef28c1a62cfc1e9017b
János Gönye <janos.gonye@gmail.com>
2019-06-20 10:51:30 +0200

    Revert "'AdaptiveCancelForTransactionCreateScreenMixin' implemented"
    This reverts commit 75eb321d02c4384e50ac01b90cac04cc077fc447.

--- app/screens/mixins.py
+++
@@ -1,6 +0,0 @@
-class AdaptiveCancelForTransactionCreateScreenMixin:
-
-    def switch_to_transaction_create_screen(self):
-        trans_create_screen = self.manager.get_screen('transaction_create')
-        trans_create_screen.back_to_screen = self.manager.current
-        self.manager.current = 'transaction_create'
