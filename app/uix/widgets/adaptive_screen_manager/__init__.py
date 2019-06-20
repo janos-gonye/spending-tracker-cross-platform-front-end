a3a155422fd2d3446c0168f4d7ce41d8a3f0905d
Gönye János <johnny@andrews.hu>
2019-06-20 11:53:44 +0200

    'AdaptiveScreenManager' now works properly

--- app/uix/widgets/adaptive_screen_manager/__init__.py
+++ app/uix/widgets/adaptive_screen_manager/__init__.py
@@ -2,15 +2,18 @@ from kivy.uix.screenmanager import ScreenManager
 
 
 class AdaptiveScreenManager(ScreenManager):
-	previously_active = None
+	history = []
 
-	def switch_to(self, screen, **options):
-		self.previously_active = self.current
-		super().switch_to(screen, **options)
+	def on_current(self, instance, value):
+		self.history.append(self.current)
+		# Don't consume memory unnecessarily
+		self.history = self.history[-2:]
+		super().on_current(instance, value)
 
 	def switch_back(self):
 		"""
 		Switch back to previously active screen.
 		Useful if you want adaptive back/cancel buttons.
+		!!!Only works if the user is only able to go back from the screen!!!
 		"""
-		self.current = self.previously_active
+		self.current = self.history[-2]
