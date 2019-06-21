09d8bb4969903fb2d62ecc1948846badf97b46e2
János Gönye <janos.gonye@gmail.com>
2019-06-21 13:01:37 +0200

    'TransactionCreateScreen' now inherits from 'TransactionSaveScreen'

--- app/screens/transaction/create/__init__.py
+++ app/screens/transaction/create/__init__.py
@@ -1,41 +1,19 @@
-from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
-
 from models.transaction import Transaction
-from screens.category.mixins import FetchCategoriesMixin
-from services.transaction import TransactionService
+from screens.transaction.save import TransactionSaveScreen
 from services.exceptions import ConnectionError_
 from uix.popups.info import InfoPopup
 
 
-Builder.load_file('screens/transaction/create/transaction_create.kv')
-
-
-class TransactionCreateScreen(FetchCategoriesMixin, Screen):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.service = TransactionService()
-
-    def on_pre_enter(self):
-        self.fetch_categories()
-
-    def on_enter(self):
-        if self.cat_conn_error:
-            InfoPopup(title='Error', message=str(self.cat_conn_error)).open()
-        else:
-            self.category_selector.init(elements=self.categories)
-            try:
-                self.category_selector.selected = self.categories[0]
-            except IndexError:
-                pass
+class TransactionCreateScreen(TransactionSaveScreen):
+    screen_title = '[b]Create Category[/b]'
+    submit_btn_text = '[b]Create[/b]'
 
     def submit(self):
         if self.check_fields() == False:
             return
         transaction = Transaction(
             id=None,
-            amount=self.number_input.text,
+            amount=self.amount_input.text,
             comment=self.comment_input.text,
             category=self.category_selector.selected,
             processed_at=self.date_picker.timestamp)
@@ -50,14 +28,7 @@ class TransactionCreateScreen(FetchCategoriesMixin, Screen):
         self.manager.switch_back()
         return created
 
-    def check_fields(self):
-        if self.number_input.text == "" or\
-           self.category_selector.selected is None:
-            InfoPopup(title='Error', message="All fields required.").open()
-            return False
-        return True
-
     def reset_fields(self):
-        self.number_input.text = ""
+        self.amount_input.text = ""
         self.comment_input.text = ""
         self.date_picker.reset_today()
