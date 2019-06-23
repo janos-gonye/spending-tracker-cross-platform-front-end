28fa89809e61a4439b45dab2f2cf60a00ddff85d
János Gönye <janos.gonye@gmail.com>
2019-06-23 20:13:53 +0200

    show statistics when pushing button with text 'See Statistics'

--- app/screens/statistics/__init__.py
+++ app/screens/statistics/__init__.py
@@ -5,12 +5,13 @@ from screens.mixins import ListScreenMixin
 from services.exceptions import ConnectionError_
 from services.statistics import StatisticsService
 from uix.popups.info import InfoPopup
+from utils.date import timestamp2datetime
 
 
 Builder.load_file('screens/statistics/statistics.kv')
 
 
-class StatisticsScreen(ListScreenMixin, Screen):
+class StatisticsScreen(Screen):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -24,7 +25,22 @@ class StatisticsScreen(ListScreenMixin, Screen):
         except ConnectionError_ as err:
             InfoPopup(title="Error", message=str(err)).open()
             return
-        print(statistics)
+        self.statistics_lbl.text = ""
+        from_ = timestamp2datetime(timestamp=from_)
+        to = timestamp2datetime(timestamp=to)
+        all_cost = self.calc_all_cost(statistics=statistics)
+        fmt = "%Y-%m-%d"
+        text = "[i]" + from_.strftime(fmt) + " -> " + to.strftime(fmt) + "\n\n"
+        text += f"All spending: {all_cost}\n\n"
+        # List main categories
+        for category in statistics:
+            text += self.statistics_for(category=category,
+                                        include_children=False)
+        text += "\n"
+        # List categories with their children detailed
+        for category in statistics:
+            text += self.statistics_for(category=category) + "\n"
+        self.statistics_lbl.text = text + '[/i]'
 
     def export_statistics(self):
         from_ = self.from_selector.timestamp
@@ -35,3 +51,23 @@ class StatisticsScreen(ListScreenMixin, Screen):
             InfoPopup(title="Error", message=str(err)).open()
             return
         InfoPopup(title="Success", message=message).open()
+
+    def statistics_for(self, category, indent=0, include_children=True):
+        key = list(category.keys())[0]
+        sum_ = int(category[key]['sum'])
+        text = ""
+        if sum_ != 0:
+            text = "{indent}{key}: {sum_}\n".format(indent=" " * indent,
+                                            key=key, sum_=sum_)
+        if include_children is True:
+            for child in category[key]['children']:
+                text += " " * indent + self.statistics_for(category=child,
+                                                           indent=indent+4)
+        return text
+
+    def calc_all_cost(self, statistics):
+        all_cost = 0
+        for category in statistics:
+            key = list(category.keys())[0]
+            all_cost += int(category[key]['sum'])
+        return all_cost
