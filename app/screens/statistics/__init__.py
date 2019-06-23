fc75761a9fd0adbba4eff5e2283682ba6038c8ce
Gönye János <janos.gonye@gmail.com>
2019-06-23 18:29:55 +0200

    export statistics button works

--- app/screens/statistics/__init__.py
+++ app/screens/statistics/__init__.py
@@ -1,6 +1,9 @@
 from kivy.lang import Builder
-
 from kivy.uix.screenmanager import Screen
+
+from services.exceptions import ConnectionError_
+from services.statistics import StatisticsService
+from uix.popups.info import InfoPopup
 
 
 Builder.load_file('screens/statistics/statistics.kv')
@@ -8,8 +11,17 @@ Builder.load_file('screens/statistics/statistics.kv')
 
 class StatisticsScreen(Screen):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.service = StatisticsService()
+
     def list_statistics(self):
         pass
 
     def export_statistics(self):
-        pass
+        try:
+            message = self.service.export_statistics()
+        except ConnectionError_ as err:
+            InfoPopup(title="Error", message=str(err)).open()
+            return
+        InfoPopup(title="Success", message=message).open()
