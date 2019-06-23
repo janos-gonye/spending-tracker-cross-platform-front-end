5667314175d151d593928056dead797a53f46c69
János Gönye <janos.gonye@gmail.com>
2019-06-23 18:29:55 +0200

    export statistics button works

--- app/services/statistics.py
+++ app/services/statistics.py
@@ -7,17 +7,17 @@ from services.exceptions import ConnectionError_
 
 class StatisticsService(ApiService):
 
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-	def get_statistics(self):
-		payload, error = super().get(path=constants.API_STATISTICS)
-		if payload is None:
-			raise ConnectionError_(error)
-		return payload['statistics']
+    def get_statistics(self):
+        payload, error = super().get(path=constants.API_STATISTICS)
+        if payload is None:
+            raise ConnectionError_(error)
+        return payload['statistics']
 
-	def export_statistics(self):
-		payload, error = super().get(path=constants.API_STATISTICS_EXPORT)
-		if payload is None:
-			raise ConnectionError_(error)
+    def export_statistics(self):
+        payload, error = super().get(path=constants.API_STATISTICS_EXPORT)
+        if payload is None:
+            raise ConnectionError_(error)
         return payload[constants.API_DEFAULT_KEY]
