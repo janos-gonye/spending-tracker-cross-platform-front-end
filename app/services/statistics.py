7d9792399482612cabde62dade7dd46904f2d9b3
János Gönye <janos.gonye@gmail.com>
2019-06-23 18:55:09 +0200

    'StatisticsService' can handle from, to params from now on, too

--- app/services/statistics.py
+++ app/services/statistics.py
@@ -7,17 +7,18 @@ from services.exceptions import ConnectionError_
 
 class StatisticsService(ApiService):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def get_statistics(self):
-        payload, error = super().get(path=constants.API_STATISTICS)
+    def get_statistics(self, from_, to):
+        params = {'from': from_, 'to': to}
+        payload, error = super().get(path=constants.API_STATISTICS,
+                                     params=params)
         if payload is None:
             raise ConnectionError_(error)
         return payload['statistics']
 
-    def export_statistics(self):
-        payload, error = super().get(path=constants.API_STATISTICS_EXPORT)
+    def export_statistics(self, from_, to):
+        params = {'from': from_, 'to': to}
+        payload, error = super().get(path=constants.API_STATISTICS_EXPORT,
+                                     params=params)
         if payload is None:
             raise ConnectionError_(error)
         return payload[constants.API_DEFAULT_KEY]
