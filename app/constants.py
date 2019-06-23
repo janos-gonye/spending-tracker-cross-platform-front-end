4a01e50d23c66a009850ba6ef3bfd906d786f05f
Gönye János <janos.gonye@gmail.com>
2019-06-23 12:01:52 +0200

    verify token path added to 'constants.py'

--- app/constants.py
+++ app/constants.py
@@ -4,6 +4,7 @@ API_DEFAULT_KEY = 'message'
 
 # API Endpoints
 API_AUTH_LOGIN = API_PATH + '/auth/login'
+API_AUTH_VERIFY_TOKEN = API_PATH + '/auth/verify-token'
 API_AUTH_REGISTRATION = API_PATH + '/auth/registration'
 API_CATEGORIES = API_PATH + '/categories'
 API_TRANACTIONS = API_PATH + '/categories/{cat_id}/transactions'
