9168889efb3f1713b495e0004bac84afbf043966
János Gönye <janos.gonye@gmail.com>
2019-07-28 17:07:05 +0200

    use 'classmethod's in 'SessionService' instead of 'staticmethod's

--- app/services/session.py
+++ app/services/session.py
@@ -9,26 +9,26 @@ class SessionService:
 	_email = None
 	_token = None
 
-	@staticmethod
-	def create(email, token):
-		SessionService._email = email
-		SessionService._token = token
-		SessionService._store.put("email", value=email)
-		SessionService._store.put("token", value=token)
+	@classmethod
+	def create(cls, email, token):
+		cls._email = email
+		cls._token = token
+		cls._store.put("email", value=email)
+		cls._store.put("token", value=token)
 
-	@staticmethod
-	def destroy():
-		SessionService._email = None
-		SessionService._token = None
-		SessionService._store.delete("email")
-		SessionService._store.delete("token")
+	@classmethod
+	def destroy(cls):
+		cls._email = None
+		cls._token = None
+		cls._store.delete("email")
+		cls._store.delete("token")
 
 	@classproperty
 	def email(cls):
 		if cls._email is not None:
 			return cls._email
 		try:
-			return SessionService._store.get("email")["value"]
+			return cls._store.get("email")["value"]
 		except KeyError:
 			return None
 
@@ -37,7 +37,7 @@ class SessionService:
 		if cls._token is not None:
 			return cls._token
 		try:
-			return SessionService._store.get("token")["value"]
+			return cls._store.get("token")["value"]
 		except KeyError:
 			return None
 
