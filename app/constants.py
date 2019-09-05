60aead029ac70f3192985dc88fc56531630aae00
Gönye János <janos.gonye@gmail.com>
2019-09-05 10:47:56 +0200

    fix change password endpoint's path

--- app/constants.py
+++ app/constants.py
@@ -6,11 +6,11 @@ API_DEFAULT_KEY = 'message'
 API_AUTH_LOGIN = API_PATH + '/auth/login'
 API_AUTH_VERIFY_TOKEN = API_PATH + '/auth/verify-token'
 API_AUTH_REGISTRATION = API_PATH + '/auth/registration'
+API_AUTH_CHANGE_PASSWORD = API_PATH + '/auth/change-password'
 API_CATEGORIES = API_PATH + '/categories'
 API_TRANACTIONS = API_PATH + '/categories/{cat_id}/transactions'
 API_STATISTICS = API_PATH + '/statistics'
 API_STATISTICS_EXPORT = API_PATH + '/statistics/export'
-API_CHANGE_PASSWORD = API_PATH + '/change-password'
 
 # Event types
 EVENT_LOGIN = 'on_login'
