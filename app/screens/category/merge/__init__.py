a55a1e17dfa73c31e0f7954d942ec381e75fe3e3
Gönye János <janos.gonye@gmail.com>
2019-09-09 17:48:45 +0200

    ask user in a popup to confirm merging categories

--- app/screens/category/merge/__init__.py
+++ app/screens/category/merge/__init__.py
@@ -1,10 +1,10 @@
 from kivy.lang import Builder
 
+from screens.category.base import CategoryScreen
 from services.category import CategoryService
 from services.exceptions import ConnectionError_
-from screens.category.base import CategoryScreen
+from uix.popups.confirm.helpers import confirm
 from uix.popups.info import InfoPopup
-
 
 Builder.load_file('screens/category/merge/category_merge.kv')
 
@@ -20,13 +20,19 @@ class CategoryMergeScreen(CategoryScreen):
         self.cat_target_select.init(elements=self.categories)
         self.service = CategoryService()
 
-    def merge(self, ):
+    def merge(self):
+        if not self.check():
+            return
+        confirm(title='Merging Categories',
+                question=(
+                    'The result of this action is permanent.\n'
+                    'Are you sure you want to continue?'),
+                confirmed=self._merge)
+
+    def _merge(self):
         subject = self.cat_subject_select.selected
         target = self.cat_target_select.selected
         remove_merged = self.remove_merged_checkbox.active
-        if None in (subject, target):
-            InfoPopup(title="Error", message="Both fields required.").open()
-            return
         try:
             self.service.merge(subject, target, remove_merged)
         except ConnectionError_ as err:
@@ -35,6 +41,14 @@ class CategoryMergeScreen(CategoryScreen):
         InfoPopup(title='Success',
                   message='Categories merged successfully.').open()
         self.manager.current = 'category_list'
+
+    def check(self):
+        subject = self.cat_subject_select.selected
+        target = self.cat_target_select.selected
+        if None in (subject, target):
+            InfoPopup(title="Error", message="Both fields required.").open()
+            return False
+        return True
 
     def reset(self):
         self.cat_subject_select.selected = None
