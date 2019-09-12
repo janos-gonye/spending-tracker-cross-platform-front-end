3cee650541b57752563159d1419fd07edd0f0d37
Gönye János <janos.gonye@gmail.com>
2019-09-12 11:49:52 +0200

    business logic added to 'ForgotPasswordScreen'

--- app/screens/forgot_password/__init__.py
+++ app/screens/forgot_password/__init__.py
@@ -1,11 +1,30 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
+from services.auth import AuthService
+from uix.popups.info import InfoPopup
+
 Builder.load_file('screens/forgot_password/forgot_password.kv')
 
 
 class ForgotPasswordScreen(Screen):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.auth = AuthService()
+
+    def submit(self):
+        email = self.email_input.text
+        if email == '':
+            InfoPopup(title='Error', message='Email field required.').open()
+            return
+        json, error = self.auth.forgot_password(email)
+        if not json:
+            InfoPopup(title='Error', message=str(error)).open()
+            return
+        InfoPopup(title='Forgot Password',
+                  message='Email to reset your password sent.').open()
+        self.manager.current = 'login'
 
     def on_leave(self):
         self.reset()
